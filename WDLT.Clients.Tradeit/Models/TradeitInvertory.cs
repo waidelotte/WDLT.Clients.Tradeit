8248b500113a2285b8bd13d256a1a6585387bf3f
waidelotte <waidelotte.advitam@gmail.com>
2021-03-10 21:22:57 +0200

    Add project files.

---
+++ WDLT.Clients.Tradeit/Models/TradeitInvertory.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using WDLT.Clients.Base;
+
+namespace WDLT.Clients.Tradeit.Models
+{
+    public class TradeitInvertory
+    {
+        [JsonProperty("730")]
+        public TradeitInvertoryApp CSGO { get; set; }
+
+        [JsonProperty("252490")]
+        public TradeitInvertoryApp Rust { get; set; }
+    }
+
+    public class TradeitInvertoryApp
+    {
+        [JsonProperty("items")]
+        public Dictionary<string, TradeitInvertoryItem> Items { get; set; }
+    }
+
+    public class TradeitInvertoryItem
+    {
+        [JsonProperty("p")]
+        [JsonConverter(typeof(LongToDoublePriceJsonConverter))]
+        public double Price { get; set; }
+
+        [JsonProperty("e")]
+        public string Quality { get; set; }
+
+        [JsonProperty("d")]
+        public List<TradeitInvertory_D> D { get; set; }
+    }
+
+    public class TradeitInvertory_D
+    {
+        [JsonProperty("f")]
+        public decimal? Float { get; set; }
+
+        [JsonProperty("r")]
+        public long? TradeLock { get; set; }
+    }
+}
---
+++ WDLT.Clients.Tradeit/TradeitClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using WDLT.Clients.Base;
+using WDLT.Clients.Tradeit.Models;
+
+namespace WDLT.Clients.Tradeit
+{
+    public class TradeitClient : BaseClient
+    {
+        public TradeitClient(string userAgent) : base("https://tradeit.gg", userAgent)
+        {
+            _client.AddDefaultHeader("referer", "https://tradeit.gg/");
+        }
+
+        public Task<List<TradeitInvertory>> Invertory(int appId)
+        {
+            return RequestAsync<List<TradeitInvertory>>(new RestRequest($"https://inventory.tradeit.gg/sinv/{appId}"));
+        }
+    }
+}
